7fae135bc85c82aa3b5c0c09dbecb3fdc73656e7
digvijaysingh21 <singh.v.digvijay@gmail.com>
2024-10-03 22:32:12 +0530

    setup backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import app from "./app.js";
+
+
+// app.listen(8000, () =>{
+//     console.log(`Server listening at port ${8000} `)
+// });
+
+
+app.listen(process.env.PORT, () =>{
+    console.log(`Server listening at port ${process.env.PORT} `);
+});
