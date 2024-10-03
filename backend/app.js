f65c432031d85b7dd3d36a0f273d74938fabb91e
digvijaysingh21 <singh.v.digvijay@gmail.com>
2024-10-03 22:57:55 +0530

    cors and middleware setup

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,39 @@
- import express from "express";
- import dotenv from "dotenv";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
- const app = express();
- dotenv.config({path: "./config/config.env"});
+const app = express();
+dotenv.config({ path: "./config/config.env" });
 //  console.log(process.env.PORT)
 
+app.use(
+  cors({
+    origin: [process.env.PORTFOLIO_URL, process.env.DASHBOARD_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
- export default app;
+// but for production level use multer
+
+/**
+ * Multer
+ * Multer is more powerful and configurable, making it a great choice for handling file uploads
+ * if you need more control over how files are stored,
+ * validated, or processed. However, the setup is a bit more involved.
+ *
+ **/
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 import app from "./app.js";
 
-
 // app.listen(8000, () =>{
 //     console.log(`Server listening at port ${8000} `)
 // });
 
-
-app.listen(process.env.PORT, () =>{
-    console.log(`Server listening at port ${process.env.PORT} `);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening at port ${process.env.PORT} `);
 });
