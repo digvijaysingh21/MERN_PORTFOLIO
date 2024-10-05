e1e0478c811cf2228815bff14248cea97cf065d2
digvijaysingh21 <singh.v.digvijay@gmail.com>
2024-10-05 14:09:47 +0530

    added getAllMessage and  delete message controller and route

--- backend/controleer/messageController.js
+++ backend/controleer/messageController.js
@@ -2,15 +2,40 @@ import { catchAsynceError } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
 import { Message } from "../models/messageSchema.js";
 
+//sending messages
 export const sendMessage = catchAsynceError(async (req, res, next) => {
-    const { senderName, subject, message } = req.body;
-    if (!senderName || !subject || !message) {
-      return next(new ErrorHandler("Please Fill Full Form!", 400));
-    }
-    const data = await Message.create({ senderName, subject, message });
-    res.status(201).json({
-      success: true,
-      message: "Message Sent",
-      data,
-    });
+  const { senderName, subject, message } = req.body;
+  if (!senderName || !subject || !message) {
+    return next(new ErrorHandler("Please Fill Full Form!", 400));
+  }
+  const data = await Message.create({ senderName, subject, message });
+  res.status(201).json({
+    success: true,
+    message: "Message Sent",
+    data,
   });
+});
+
+//get all messages
+export const getAllMessages = catchAsynceError(async (req, res, next) => {
+  const messages = await Message.find();
+  res.status(200).json({
+    success: true,
+    messages,
+  });
+});
+
+//delete messages
+
+export const deleteMessage = catchAsynceError(async (req, res, next) => {
+  const { id } = req.params;
+  const message = await Message.findById(id);
+  if (!message) {
+    return next(new ErrorHandler("Message Already Deleted", 400));
+  }
+  await message.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Message Deleted",
+  });
+});
--- backend/router/messageRoute.js
+++ backend/router/messageRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { sendMessage } from "../controleer/messageController.js";
+import { getAllMessages, sendMessage } from "../controleer/messageController.js";
 
 const router = express.Router();
 
 router.post("/send", sendMessage);
+router.get("/getall", getAllMessages);
 
 export default router;
