ffc0ecaea643ef159580ab010895dd549f8f5131
digvijaysingh21 <singh.v.digvijay@gmail.com>
2024-10-05 22:49:30 +0530

    added login feature

--- backend/controler/userControler.js
+++ backend/controler/userControler.js
@@ -2,6 +2,7 @@ import {catchAsynceError} from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
 import {User} from "../models/userSchema.js";
 import {v2 as cloudinary} from "cloudinary";
+import { generateToken } from "../utils/jwtToken.js";
 
 export const register = catchAsynceError(async(req, res, next) => {
     if(!req.files || Object.keys(req.files).length === 0){
@@ -10,7 +11,7 @@ export const register = catchAsynceError(async(req, res, next) => {
 
     const {avatar} = req.files;
 
-    console.log("AVATARavatar", avatar);
+    // console.log("AVATARavatar", avatar);
 
     const cloudinaryResponseForAvatar = await cloudinary.uploader.upload(
         avatar.tempFilePath,
@@ -26,7 +27,7 @@ export const register = catchAsynceError(async(req, res, next) => {
 
     const {resume} = req.files;
 
-    console.log("MY_RESUME", resume);
+    // console.log("MY_RESUME", resume);
 
     const cloudinaryResponseForResume = await cloudinary.uploader.upload(
         resume.tempFilePath,
@@ -76,8 +77,27 @@ export const register = catchAsynceError(async(req, res, next) => {
         },
     });
 
-    res.status(200).json({
-        success: true,
-        message: "User Registered",
-    });
+    // res.status(200).json({
+    //     success: true,
+    //     message: "User Registered",
+    // });
+
+    generateToken(user, "User Registered", 201, res);
+});
+
+
+export const login = catchAsynceError(async(req, res, next)=>{
+    const {email, password}  = req.body;
+    if(!email || !password){
+        return next(new ErrorHandler("Email And Password Are Required", 400));
+    }
+    const user = await User.findOne({email}).select("+password");
+    if(!user){
+        return next(new ErrorHandler("Invalid Email Or Password",404));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid Email Or Password",401));
+    }
+    generateToken(user, "Logged In Successfully", 200, res);
 });
--- backend/router/userRoutes.js
+++ backend/router/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {register } from "../controler/userControler.js";
+import {register, login } from "../controler/userControler.js";
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 
 export default router;
